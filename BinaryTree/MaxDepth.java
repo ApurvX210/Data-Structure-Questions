5e659993b1a26783ead0d7af45bcb4ccd4d66f01
ApurvX210 <apurvsharma210@gmail.com>
2023-03-17 09:39:46 +0530

    TwentySecond Commit

---
+++ BinaryTree/MaxDepth.java
@@ -0,0 +1,27 @@
+public class MaxDepth {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Integer.max(maxDepth(root.left) + 1, maxDepth(root.right) + 1);
+    }
+}
