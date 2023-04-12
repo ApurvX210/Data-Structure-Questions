91502cce35675c8aa131edbfab9c05b7d738045b
ApurvX210 <apurvsharma210@gmail.com>
2023-04-12 08:46:28 +0530

    Thirt Eight Commit

---
+++ BinaryTree/SameTree.java
@@ -0,0 +1,39 @@
+public class SameTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null && q==null){
+            return true;
+        }
+        if(p==null && q!=null){
+            return false;
+        }
+        if(p!=null && q==null){
+            return false;
+        }
+        if(p.val==q.val){
+            if(isSameTree(p.left, q.left) && isSameTree(p.right, q.right)){
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+}
