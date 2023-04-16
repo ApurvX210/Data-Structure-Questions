e4492895598465738a8248a2932b535abf5a00cd
ApurvX210 <apurvsharma210@gmail.com>
2023-04-16 19:46:07 +0530

    Fourty Two Commit

---
+++ BinaryTree/BoundaryTraversal.java
@@ -0,0 +1,18 @@
+public class BoundaryTraversal {
+    class Node {
+        int data;
+        Node left, right;
+
+        public Node(int d) {
+            data = d;
+            left = right = null;
+        }
+    }
+    public void traversal(Node node){
+        
+    }
+
+    ArrayList<Integer> boundary(Node node) {
+
+    }
+}
